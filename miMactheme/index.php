5a1318f02fa4fb8f933bb1020ca7b0300520b137
Alberto Suárez Inclán <37137810+albsuarez@users.noreply.github.com>
2022-06-29 10:01:58 +0200

    Add files via upload

---
+++ miMactheme/index.php
@@ -0,0 +1,353 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <!-- Style CSS -->
+  <link rel="stylesheet" href="wp-content/themes/miMactheme/style.css" crossorigin="anonymous">
+
+  <!-- meta -->
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <META HTTP-EQUIV='content-type' CONTENT='text/html;charset=UTF-8'>
+</head>
+
+<body>
+
+  <!-- HEADER -->
+  <header>
+    <div class="container-fluid w-100" id="inicio">
+      <div class="row p-2">
+        <div class="header-col-rt col-md-5 text-center">
+          <img class="logo-header" src="wp-content/themes/miMactheme/icons/Shape-black.png" alt="back-logo">
+        </div>
+        <div class="header-col-lf col-md-1 mt-3">
+          <a href="#inicio">Inicio</a>
+        </div>
+        <div class="header-col-lf col-md-1 mt-3">
+          <a href="#servicios">Servicios</a>
+        </div>
+        <div class="header-col-lf col-md-1 mt-3">
+          <a href="#quienes-somos">Quienes somos</a>
+        </div>
+        <div class="header-col-lf col-md-1 mt-3">
+          <a href="#contacto">Contacto</a>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- / HEADER -->
+
+  <!-- SECTION 1 -->
+  <section class="section-1 mb-5">
+    <div class="container py-lg-2">
+      <div class="row flex-lg-row-reverse flex-column-reverse align-items-center">
+        <div class="col-12 col-md-4 col-lg-6">
+          <div class="img-container-1">
+            <img src="wp-content/themes/miMactheme/images/MacBook-Pro.png" alt="Macbook Pro">
+          </div>
+        </div>
+        <div class="col-12 col-md-7 col-lg-6 py-3">
+          <h1 class="title text-left">MiMac</h1>
+          <p class="subtitte-home">Reparación, reacondicionamiento y alquiler de equipos de Apple</p>
+          <div>
+            <!-- <button type="button" class="btn btn-secondary btn-lg text-uppercase" href="#contacto">Contactar</button> -->
+            <a class="btn btn-secondary btn-lg text-uppercase" href="#contacto" role="button">Contactar</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--/ SECTION 1 -->
+
+  <!-- SECTION 2 - CARDS -->
+  <section class="section-2 pt-5 pb-5">
+    <div class="container" id="servicios">
+      <h2 class="section-title">Nuestros Servicios</h2>
+      <div class="row pt-2">
+        <!-- topic -->
+        <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+          <div class="card match-height" style="height: 290.188px;">
+            <div class="card-body text-center">
+              <img src="wp-content/themes/miMactheme/icons/laptop.png" alt="icon-laptop">
+              <h3 class="card-title h4">Reparación de</h3>
+              <p class="card-text">iMac y MacBook</p>
+            </div>
+          </div>
+        </div>
+        <!-- topic -->
+        <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+          <div class="card match-height" style="height: 290.188px;">
+            <div class="card-body text-center">
+              <img src="wp-content/themes/miMactheme/icons/tablet-alt.png" alt="icon-laptop">
+              <h3 class="card-title h4">Reparación de</h3>
+              <p class="card-text">iPhone y iPad</p>
+            </div>
+          </div>
+        </div>
+        <!-- topic -->
+        <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+          <div class="card match-height" style="height: 290.188px;">
+            <div class="card-body text-center">
+              <img src="wp-content/themes/miMactheme/icons/calendar-alt.png" alt="icon-laptop">
+              <h3 class="card-title h4">Servicio de</h3>
+              <p class="card-text">Alquiler de equipos</p>
+            </div>
+          </div>
+        </div>
+        <!-- topic -->
+        <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+          <div class="card match-height" style="height: 290.188px;">
+            <div class="card-body text-center">
+              <img src="wp-content/themes/miMactheme/icons/truck.png" alt="icon-laptop">
+              <h3 class="card-title h4">Servicio de</h3>
+              <p class="card-text">Recogida y envío de equipos</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- / SECTION 2 - CARDS -->
+
+  <!-- SECTION 3 -->
+  <section class="section-3 pb-5">
+    <div class="container" id="quienes-somos">
+      <div class="row flex-lg-row-reverse flex-column-reverse align-items-center">
+        <div class="col-12 col-md-4 col-lg-6">
+          <div class="img-container-2">
+            <img src="wp-content/themes/miMactheme/images/quienes-somos.jpg" alt="quienes-somos">
+          </div>
+        </div>
+        <div class="col-12 col-md-7 col-lg-6 body-text">
+          <h2 class="section-title">Quienes Somos</h2>
+          <p class="subtite-home pt-3">Somos una empresa de Madrid, la mayoría de empresas pequeñas y medianas de
+            nuestro alrededor confía en nosotros para el mantenimiento de sus ordenadores Mac. Hemos trabajado <b>con
+              todo tipo de empresas</b>, sobretodo agencias de publicidad y diseño, productoras audiovisuales y nuevas
+            startups.
+            Si buscas alguien en quien poder confiar el mantenimiento de tus equipos estás en el lugar adecuado.</p>
+          <div>
+            <p>&#8226; &nbsp; Llevamos 15 años reparando ordenadores Apple.</p>
+            <p>&#8226; &nbsp; Más de 1000 dispositivos arreglados.</p>
+            <p>&#8226; &nbsp; Alrededor de 150 clientes satisfechos con nuestro servicio.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- / SECTION 3 -->
+
+  <!-- SECTION 4 -->
+  <div class="section-4 d-flex contact-us-section pt-5 pb-5">
+    <div class="container" id="contacto">
+      <div class="row py-4 py-lg-0">
+        <div class="col-lg-5 offset-lg-3">
+          <h2 class="section-title">Contacto</h2>
+          <div class="title-separator" id='crmWebToEntityForm'></div>
+          <p>Si te interesan nuestros servicios no dudes en contactar con nosotros. Puedes escribirnos a 
+            <a class="contact-link" href="mailto:info@mimac.es">info@mimac.es</a>, llamarnos al teléfono 
+            <a class="contact-link" href="callto:info@mimac.es">123 456 789</a> o utilizar el siguiente formulario:</p>
+          <form action='https://crmsandbox.zoho.eu/crm/WebToContactForm' name=WebToContacts381347000000611007
+            method='POST' onSubmit='javascript:document.charset="UTF-8"; return checkMandatory381347000000611007()'
+            accept-charset='UTF-8'>
+            <input type='text' style='display:none;' name='xnQsjsdp'
+              value='2fdff2e0e3cdfea6982630a4d00c7427ea401a1792f16c1282ae7004fcd398e7'></input>
+            <input type='hidden' name='zc_gad' id='zc_gad' value=''></input>
+            <input type='text' style='display:none;' name='xmIwtLD'
+              value='aafd04f8be021291b5f977c594dfc790b55d6e51950614e1ca1e73a2c5f58b05'></input>
+            <input type='text' style='display:none;' name='actionType' value='Q29udGFjdHM='></input>
+            <input type='text' style='display:none;' name='returnURL'
+              value='http&#x3a;&#x2f;&#x2f;dominioprueba-asi.link&#x2f;'> </input>
+            <div class="form-group">
+              <label for="Last_Name">Nombre</label>
+              <input type="name" class="form-control" id='Last_Name' name='Last Name' maxlength='80'>
+            </div>
+            <div class="form-group">
+              <label for="Email">Email</label>
+              <input type="email" class="form-control" id='Email' name='Email' maxlength='100'>
+            </div>
+            <div class="form-group">
+              <label for="Phone">Teléfono</label>
+              <input type="phone" class="form-control" id='Phone' name='Phone' maxlength='50'>
+            </div>
+            <div class="form-group">
+              <label for="Description">Mensaje</label>
+              <textarea class="form-control" id='Description' name='Description' rows="3"></textarea>
+            </div>
+            <div class="form-group">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="privacyCheck">
+                <label class="form-check-label" for="privacyCheck">
+                  Acepto la política de privacidad
+                </label>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="newsletterCheck">
+                <label class="form-check-label" for="newsletterCheck">
+                  Quiero apuntarme a la newsletter
+                </label>
+              </div>
+            </div>
+            <div class="form-group text-center">
+              <div class='zcwf_row'>
+                <div class='zcwf_col_lab'></div>
+                <div class='zcwf_col_fld'><img id='imgid381347000000611007'
+                    src='https://crmsandbox.zoho.eu/crm/CaptchaServlet?formId=aafd04f8be021291b5f977c594dfc790b55d6e51950614e1ca1e73a2c5f58b05&grpid=2fdff2e0e3cdfea6982630a4d00c7427ea401a1792f16c1282ae7004fcd398e7'>
+                  <a href='javascript:;' onclick='reloadImg381347000000611007();'>Reload</a>
+                </div>
+                <div class=''></div>
+              </div>
+            </div>
+              <div class="form-group">
+                <label for="Last_Name">Captcha</label>
+                <input type="text" class="form-control" name='enterdigest' maxlength='10'>
+              </div>
+              <button type="submit" id='formsubmit' class="btn btn-secondary btn-lg text-uppercase w-100">Enviar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- / SECTION 4 -->
+
+
+  <!-- Do not remove this code. -->
+  <!-- </div><div class='zcwf_row'><div class='zcwf_col_lab'></div><div class='zcwf_col_fld'><input type='submit' id='formsubmit' class='formsubmit zcwf_button' value='Submit' title='Submit'><input type='reset' class='zcwf_button' name='reset' value='Reset' title='Reset'></div></div> -->
+  <script>
+
+    /* Do not remove this code. */
+    function reloadImg381347000000611007() {
+      var captcha = document.getElementById('imgid381347000000611007');
+      if (captcha.src.indexOf('&d') !== -1) {
+        captcha.src = captcha.src.substring(0, captcha.src.indexOf('&d')) + '&d' + new Date().getTime();
+      } else {
+        captcha.src = captcha.src + '&d' + new Date().getTime();
+      }
+    }
+    function validateEmail381347000000611007() {
+      var form = document.forms['WebToContacts381347000000611007'];
+      var emailFld = form.querySelectorAll('[ftype=email]');
+      var i;
+      for (i = 0; i < emailFld.length; i++) {
+        var emailVal = emailFld[i].value;
+        if ((emailVal.replace(/^\s+|\s+$/g, '')).length != 0) {
+          var atpos = emailVal.indexOf('@');
+          var dotpos = emailVal.lastIndexOf('.');
+          if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= emailVal.length) {
+            alert('Please enter a valid email address. ');
+            emailFld[i].focus();
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+
+    function checkMandatory381347000000611007() {
+      var checkbox = document.querySelector('#privacyCheck').checked;
+      var checkbox2 = document.querySelector('#newsletterCheck').checked;
+
+      if (checkbox == false) {
+          alert('You must agree to the terms first.');
+            return false;
+      }
+      
+      if (checkbox2 == false) {
+        alert('You must agree to the terms first.');
+          return false;
+      }
+
+      var mndFileds = new Array('Last Name', 'Email', 'Description');
+      var fldLangVal = new Array('Nombre', 'Email', 'Mensaje');
+      for (i = 0; i < mndFileds.length; i++) {
+        var fieldObj = document.forms['WebToContacts381347000000611007'][mndFileds[i]];
+        if (fieldObj) {
+          if (((fieldObj.value).replace(/^\s+|\s+$/g, '')).length == 0) {
+            if (fieldObj.type == 'file') {
+              alert('Please select a file to upload.');
+              fieldObj.focus();
+              return false;
+            }
+            alert(fldLangVal[i] + ' cannot be empty.');
+            fieldObj.focus();
+            return false;
+          } else if (fieldObj.nodeName == 'SELECT') {
+            if (fieldObj.options[fieldObj.selectedIndex].value == '-None-') {
+              alert(fldLangVal[i] + ' cannot be none.');
+              fieldObj.focus();
+              return false;
+            }
+          } else if (fieldObj.type == 'checkbox') {
+            if (fieldObj.checked == false) {
+              alert('Please accept  ' + fldLangVal[i]);
+              fieldObj.focus();
+              return false;
+            }
+          }
+          try {
+            if (fieldObj.name == 'Last Name') {
+              name = fieldObj.value;
+            }
+          } catch (e) { }
+        }
+      }
+      if (!validateEmail381347000000611007()) { return false; }
+      document.querySelector('.crmWebToEntityForm .formsubmit').setAttribute('disabled', true);
+    }
+
+    function tooltipShow381347000000611007(el) {
+      var tooltip = el.nextElementSibling;
+      var tooltipDisplay = tooltip.style.display;
+      if (tooltipDisplay == 'none') {
+        var allTooltip = document.getElementsByClassName('zcwf_tooltip_over');
+        for (i = 0; i < allTooltip.length; i++) {
+          allTooltip[i].style.display = 'none';
+        }
+        tooltip.style.display = 'block';
+      } else {
+        tooltip.style.display = 'none';
+      }
+    }
+  </script>
+  <!-- Do not remove this --- Analytics Tracking code starts -->
+  <script id='wf_anal'
+    src='https://crm.zohopublic.eu/crm/WebFormAnalyticsServeServlet?rid=aafd04f8be021291b5f977c594dfc790b55d6e51950614e1ca1e73a2c5f58b05gid2fdff2e0e3cdfea6982630a4d00c7427ea401a1792f16c1282ae7004fcd398e7gidea93f4322d46ee698fe0c65bddf10eabbc5de3875e0e613a51d77e7d4100e249gidc4408f450319003e0c12e54df63a8632&tw=cbfb750ab7d38d47a14b773ee2feb5d02289cbd83eea2c768228fc69ce2034ab'></script>
+  <!-- Do not remove this --- Analytics Tracking code ends. -->
+  </form>
+  <!-- Do not remove this code. -->
+  <iframe name='captchaFrame' style='display:none;'></iframe>
+  </div>
+
+
+  <!-- FOOTER -->
+  <footer>
+    <div class="container-fluid w-100 p-5" style="background-image: url('wp-content/themes/miMactheme/images/Rectangle.png');">
+      <div class="row">
+        <div class="footer-col-rt col-md-6 text-center">
+          <img src="wp-content/themes/miMactheme/icons/Shape.png" alt="">
+        </div>
+        <div class="footer-col-lf col-md-1 mt-3 text-center">
+          <a href="#inicio">Inicio</a>
+        </div>
+        <div class="footer-col-lf col-md-1 mt-3 text-center">
+          <a href="#servicios">Servicios</a>
+        </div>
+        <div class="footer-col-lf col-md-1 mt-3 text-center">
+          <a href="#quienes-somos">Quienes somos</a>
+        </div>
+        <div class="footer-col-lf col-md-1 mt-3 text-center">
+          <a href="#contacto">Contacto</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- / FOOTER -->
+</body>
+
+</html>
